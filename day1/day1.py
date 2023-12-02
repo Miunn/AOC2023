56e674a69b6028b9471af2d46a3523474e68e57e
Miunn <remcaulier@orange.fr>
2023-12-02 12:05:13 +0100

    Day 1 P2

--- day1/day1.py
+++ day1/day1.py
@@ -1,17 +1,62 @@
 with open('input.txt', 'r') as f:
     lines = [l.replace('\n', '') for l in f.readlines()]
 
+
+def literalToNumber(literal):
+    if literal == 'one':
+        return '1'
+    if literal == 'two':
+        return '2'
+    if literal == 'three':
+        return '3'
+    if literal == 'four':
+        return '4'
+    if literal == 'five':
+        return '5'
+    if literal == 'six':
+        return '6'
+    if literal == 'seven':
+        return '7'
+    if literal == 'eight':
+        return '8'
+    if literal == 'nine':
+        return '9'
+
+    return ''
+
+def searchLiteral(literals, c, reverse=False):
+    for i in range(len(literals)):
+        literals[i] += c
+        conv = literalToNumber(literals[i] if not reverse else literals[i][::-1])
+        if conv != '':
+            return (literals, conv)
+    
+    literals.append(c)
+    
+    return (literals, '')
+
 output = 0
 for l in lines:
     value = ''
+    literals = []
     for c in l:
+        literals, converted = searchLiteral(literals, c)
+        if converted != '':
+            value += converted
+            break
+
         try:
             value = str(int(c))
             break;
         except:
             continue
     
+    literals = []
     for c in l[::-1]:
+        literals, converted = searchLiteral(literals, c, reverse=True)
+        if converted != '':
+            value += converted
+            break
         try:
             value += str(int(c))
             break;
